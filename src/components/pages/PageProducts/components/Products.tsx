8d603cb4f9b74175bcd182788e51603435b4f893
Andrii Veldymanov <Andrii_Veldymanov@epam.com>
2021-04-26 01:06:15 +0300

    task-4: small improvements

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -35,7 +35,7 @@ export default function Products() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    axios.get(`${API_PATHS.product}/product-list/`)
+    axios.get(`${API_PATHS.product}/products/`)
       .then(res => {
         setProducts(res.data.products);
       })
