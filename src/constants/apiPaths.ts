bbfe616d3ad5b8395fb75bb54e495cc0c5b68032
Andrii Veldymanov <Andrii_Veldymanov@epam.com>
2021-06-30 15:50:37 +0300

    task-8: update paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://8vr2vt65eb.execute-api.eu-west-1.amazonaws.com/dev',
   bff: ' https://i7pjln1loi.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://my-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
